3459df0b245731dfc53b7ffad53f52fbf943098e
unknown <codeleap@outlook.com>
2019-02-16 14:40:52 +0100

    Added assigning controller and action properties in Bootstrap class and tested.

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -1,18 +1,23 @@
 <?php
 class Bootstrap{
   private $request;
+  private $controller;
+  private $action;
 
   public function __construct($request){
-     //$request function is an array based on $_GET taken from browser input. 'application/controller/action/id'
-     $this->request = $request;   
+     //$request parameter is an associative array based on $_GET taken from browser input. 'application/controller/action/id'
+     $this->request = $request;
+     //test if controller is blank, if so set controller property to 'home'.
+     if($this->request['controller'] == ""){
+        $this->controller = 'home';
+     }else{
+        $this->controller = $this->request['controller'];
+     }
   }
   
   public function test(){
-    if($this->request['controller'] == '') echo "Controller is empty";
-    echo $this->request['controller'] . "<br>";
-    echo $this->request['action'] . "<br>";
-    if($this->request['id'] == '') echo "Id is empty";
-    echo $this->request['id'] . "<br>";
+    print_r($this->request);
+    echo "<br>" . $this->controller . "<br>" . $this->action;
   }
 
 }
