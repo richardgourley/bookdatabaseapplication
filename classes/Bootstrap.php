5764afa392e65bced4e297dac13ea9bc1b9cda17
unknown <codeleap@outlook.com>
2019-02-18 17:29:27 +0100

    Added create controller class to Bootstrap.php. Tested working.

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -5,19 +5,47 @@ class Bootstrap{
   private $action;
 
   public function __construct($request){
-     //$request parameter is an associative array based on $_GET taken from browser input. 'application/controller/action/id'
      $this->request = $request;
-     //test if controller is blank, if so set controller property to 'home'.
+
+     //If request is blank ie. site home page, $controller = 'home', $action = 'index'
      if($this->request['controller'] == ""){
         $this->controller = 'home';
      }else{
         $this->controller = $this->request['controller'];
      }
+
+     //If request['action'] is blank, set to index. Eg 'bookdatabaseapplication/books' = $controller = 'books', $action = 'index'
+     if($this->request['action'] == ""){
+        $this->action = 'index';
+     }else{
+        $this->action = $request['action'];
+     }
+
   }
   
-  public function test(){
-    print_r($this->request);
-    echo "<br>" . $this->controller . "<br>" . $this->action;
+  /*
+  * @ return = returns either a new instance of a controller based on controller name and action OR...
+  * ... prints an error message and returns if controller OR action does not exist.
+  *
+  */
+  public function create_controller(){
+     if(class_exists($this->controller)){ 
+        //Next 2 lines - Gets classes $this->controller extends from (array). Then we check 'Controller' base class exists and $this->controller extends from it.
+        $parents = class_parents($this->controller); 
+        if(in_array('Controller', $parents)){ 
+           if(method_exists($this->controller, $this->action)){ 
+              return new $this->controlller($this->action, $this->request); 
+           }else{
+              echo "Method " . $this->action . " doesn't exist in class " . $this->controller . "."; 
+           }
+        }else{
+           echo "Class named " . $this->controller . " found but base controller class not found."; 
+           return;
+        }
+     }else{
+        echo "Class named " . $this->controller . " not found."; 
+        return;
+     }
   }
 
 }
