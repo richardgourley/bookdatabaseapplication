958b0c3c79babc3b65d0f297e9da0522968ad711
unknown <codeleap@outlook.com>
2019-02-20 15:22:25 +0100

    controllers/home.php - Index function added and tested - working correctly.

--- controllers/home.php
+++ controllers/home.php
@@ -1,4 +1,7 @@
 <?php
 class Home extends Controller{
-	
+	//Called by Controller function execute_action().
+	public function Index(){
+		echo "Hello I am index function inside the Home class.";
+	}
 }
