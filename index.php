5a6813da0a9e98a9cc6d2d0c4ef7d7c1436fa8d5
unknown <codeleap@outlook.com>
2019-02-20 15:07:12 +0100

    Controller base file added to index. Controller home added to index.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require_once('config.php');
 //get bootstrap class that creates the controller.
 require_once('classes/bootstrap.php');
+require_once('classes/Controller.php');
+require_once('controllers/home.php');
 
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->create_controller();
