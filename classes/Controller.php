2d288258ff6d825932d00d02c15380006b933b3c
unknown <codeleap@outlook.com>
2019-02-20 15:11:37 +0100

    Base Controller file - added fullview option to return_view function.

--- classes/Controller.php
+++ classes/Controller.php
@@ -16,7 +16,6 @@ abstract class Controller{
    
    //Returns desired html view. $viewmodel comes from model. $fullview is a bool
    protected function return_view($viewmodel, $fullview){
-      
       $view = 'views/' . get_class($this) . '/' . $this->action . '.php'; //Var with link to the VIEW for this ACTION
       if($fullview){
          return ('views/main.php'); //Alternative main.php used if $fullview = true
