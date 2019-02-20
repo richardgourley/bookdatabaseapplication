b29b618942a817af952a8205a21077f324f6783c
unknown <codeleap@outlook.com>
2019-02-20 06:53:15 +0100

    Comments added to return_view function.

--- classes/Controller.php
+++ classes/Controller.php
@@ -14,13 +14,12 @@ abstract class Controller{
       return $this->{$this->action}();
    }
    
-   //Returns desired html view. $viewmodel comes from model. $fullview is a bool that directs to main page view (full) or view specific to this model.
+   //Returns desired html view. $viewmodel comes from model. $fullview is a bool
    protected function return_view($viewmodel, $fullview){
-      //Create a variable that contains the link to the view created for this action.
-      $view = 'views/' . get_class($this) . '/' . $this->action . '.php';
-      //Full view logic.
+      
+      $view = 'views/' . get_class($this) . '/' . $this->action . '.php'; //Var with link to the VIEW for this ACTION
       if($fullview){
-         return ('views/main.php');
+         return ('views/main.php'); //Alternative main.php used if $fullview = true
       }else{
          return $view;
       }
